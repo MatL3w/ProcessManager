0d2eb59133177b869053a144c65a6897437c97a6
Lecislav <lecislav@protonmail.com>
2023-07-28 10:03:33 +0200

    Animations in Angular

--- ProcessManager/src/app/data/data.component.ts
+++ ProcessManager/src/app/data/data.component.ts
@@ -4,14 +4,37 @@ import { Observable, Subscription } from 'rxjs';
 import { change } from '../store/data.actions';
 import { DataInterface } from '../store/data.reducer';
 import { selectProcessId } from '../store/data.selectors';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   standalone: true,
   selector: 'app-data',
   templateUrl: './data.component.html',
   styleUrls: ['./data.component.css'],
+  animations: [
+    trigger('pState',
+    [
+      state(
+        'normal',
+        style({
+          'background-color': 'red',
+          transform: 'translateX(0)',
+        })
+      ),
+      state(
+        'highlighted',
+        style({
+          'background-color': 'white',
+          transform: 'translateX(100px)',
+        })
+      ),
+      transition('normal => highlighted', animate(300)),
+      transition('normal => highlighted', animate(300)),
+    ]),
+  ],
 })
 export class DataComponent {
+  state = 'normal';
   procesId = signal(0);
   data$!: Observable<Object>;
   data$subscription!: Subscription;
@@ -31,9 +54,14 @@ export class DataComponent {
 
   clickChangeProcessId() {
     this.GenerateNewProcessId();
+    this.state == 'normal'
+      ? (this.state = 'highlighted')
+      : (this.state = 'normal');
   }
 
   GenerateNewProcessId() {
-    this.store.dispatch(change({ value: Number((Math.random() * 1000000).toFixed(0)) }));
+    this.store.dispatch(
+      change({ value: Number((Math.random() * 1000000).toFixed(0)) })
+    );
   }
 }
--- ProcessManager/src/main.ts
+++ ProcessManager/src/main.ts
@@ -6,13 +6,15 @@ import { provideStore } from '@ngrx/store';
 import { dataReducer } from './app/store/data.reducer';
 import { provideEffects } from '@ngrx/effects';
 import { DataEffects } from './app/store/data.effects';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-bootstrapApplication(AppComponent,{
+bootstrapApplication(AppComponent, {
   providers: [
-    importProvidersFrom(AppRoutingModule),
+    importProvidersFrom([AppRoutingModule,BrowserModule,BrowserAnimationsModule]),
     provideStore({
-        data: dataReducer,
+      data: dataReducer,
     }),
-    provideEffects([DataEffects])
-  ]
+    provideEffects([DataEffects]),
+  ],
 });
